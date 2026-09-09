7b2605d5542a28c9a014348d13a178d4eb5f3f33
Pablo <pablo.a.lopes@aluno.senai.br>
2026-09-09 11:24:39 -0300

    CORS criado

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
-// const tarefas = [{texto: 'estudar', prioridade: 'alta', id: 1,}]
-const PORTA = 3000;
+const PORTA = process.env.PORTA || 3000;
 const tarefasRoutes = require('./src/tarefas/tarefas.routes');
 const usuarioRoutes = require('./src/usuarios/usuarios.routes');
 const logger = require('./src/middleware/logger');
+// const corsMiddleware = require('./src/middleware/cors');
 const validarContentType = require('./src/middleware/validarContentType');
 
-
+// app.use(corsMiddleware);
 app.use(express.json()) // PARTE IMPORTANTE PARA QUE O GET - PUT - DELET - POST POSSAM FUNCIONAR
 app.use(validarContentType);
 app.use(logger);
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || 'https://www.google.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    maxAge: 86400,
+}));
+
+app.prependOnceListener(PORTA, () => {
+    console.log(`Servidor na porta ${PORTA}`);
+})
 
 // PRODUÇÃO
 

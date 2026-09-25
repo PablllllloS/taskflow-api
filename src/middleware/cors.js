6889dff9a1f68479fa28d7a5d8b46bda9f4483db
Pablo <pablo.a.lopes@aluno.senai.br>
2026-09-25 10:37:58 -0300

    ajuste no cors

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,6 +1,6 @@
 function corsMiddleware(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', ["GET", "POST", "PUT", "DELETE", "OPTIONS"]);
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Max-Age', '86400');
 
